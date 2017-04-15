f15660123001f5e0980039325105f96c4a363bac
Erika Sankari <8eikochan@gmail.com>
2017-04-15 17:19:34 +0300

    Add possibility to delete reviews.

--- src/main/java/com/evil/Controller/ReviewController.java
+++ src/main/java/com/evil/Controller/ReviewController.java
@@ -4,6 +4,7 @@ import com.evil.Entity.Product;
 import com.evil.Entity.Review;
 import com.evil.Entity.User;
 import com.evil.Exception.IllegalReviewException;
+import com.evil.Exception.NoReviewsException;
 import com.evil.Repository.ProductRepository;
 import com.evil.Repository.ReviewRepository;
 import com.evil.Repository.UserRepository;
@@ -73,5 +74,40 @@ public class ReviewController {
         productRepository.save(product);
 
         return ResponseEntity.status(201).body(review);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Review> deleteReview(@PathVariable("id") int id) {
+
+        String userdetails = (String) SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        User user = userRepository.findByEmail(userdetails);
+        Product product = productRepository.findOne(id);
+        List<Review> reviews = product.getReviews();
+
+        // checks if product has reviews.
+        if (product.getReviews().size() > 0) {
+            Review reviewToDelete = null;
+            // loops reviews to check that user's id matches with the review.
+            // User can delete only his/her reviews.
+            for (Review review1 : reviews) {
+                if (review1.getUser().getId() == user.getId()) {
+                    reviewToDelete = review1;
+                }
+            }
+
+            if (reviewToDelete != null) {
+                reviews.remove(reviewToDelete);
+                reviewRepository.delete(reviewToDelete);
+                product.setReviews(reviews);
+                productRepository.save(product);
+
+                return ResponseEntity.ok(reviewToDelete);
+            } else {
+                throw new NoReviewsException();
+            }
+        }
+
+        throw new NoReviewsException();
     }
 }
