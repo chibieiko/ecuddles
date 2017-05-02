154df71ae2fa6f005c55e63a225f2ab25c8833a7
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-05-02 10:18:17 +0300

    Enable CORS.

--- src/main/java/com/evil/EcuddlesApplication.java
+++ src/main/java/com/evil/EcuddlesApplication.java
@@ -3,13 +3,28 @@ package com.evil;
 import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEmailTools
 public class EcuddlesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EcuddlesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTION")
+						.allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 }
