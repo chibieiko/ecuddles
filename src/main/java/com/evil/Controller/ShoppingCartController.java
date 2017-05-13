861d10edb49db825e1220f20413274354be4e6d5
Vili Kinnunen <vili.kinnunen1@gmail.com>
2017-05-13 18:19:54 +0300

    Fix out of stock exception being thrown when it shouldnt.

--- src/main/java/com/evil/Controller/ShoppingCartController.java
+++ src/main/java/com/evil/Controller/ShoppingCartController.java
@@ -106,10 +106,6 @@ public class ShoppingCartController {
                 boolean updated = false;
                 for (ShoppingCartItem shoppingCartItem : userCart) {
                     if (shoppingCartItem.getProduct() == product) {
-                        if (product.getStock() < shoppingCartItem.getQuantity() + quantity) {
-                            throw new OutOfStockException();
-                        }
-
                         shoppingCartItem.setQuantity(quantity);
                         updated = true;
                     }
